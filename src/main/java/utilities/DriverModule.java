32ee45292ca88a5c4d33d8697e20774c318f6871
Saha <gsaha@wiley.com>
2023-04-12 18:09:47 +0530

    Driver module modification for BS

--- src/main/java/utilities/DriverModule.java
+++ src/main/java/utilities/DriverModule.java
@@ -27,15 +27,15 @@ import com.browserstack.local.Local;
 public class DriverModule {
 
 	public  static WebDriver driver =null;
-	public static final String username="gourabsaha_Nlruz2";
-	public static final String AccessToken ="WUrEEqUxwLoVAJEKWbyh";
+	public static final String username		= excelOperation.getTestData("BS_Username", "BrowserStack_Config", "Data");
+	public static final String AccessToken 	= excelOperation.getTestData("BS_Password", "BrowserStack_Config", "Data");
 	
 	public static final String URL = "https://"+username+":"+AccessToken+"@hub-cloud.browserstack.com/wd/hub";
 	
 	
 	@BeforeTest
-	@Parameters("browser")
-	public void initiate(ITestContext context,@Optional("chrome") String browser)
+	@Parameters({"browser","DeviceType"})
+	public void initiate(ITestContext context,@Optional("chrome") String browser,@Optional("Desktop")String DeviceType)
 	{
 		try {
 			
@@ -44,70 +44,75 @@ public class DriverModule {
 			String testSuiteName=context.getCurrentXmlTest().getClasses().stream()
 		               .findFirst().get().getName().substring(11);
 			
-		/*	DesiredCapabilities Caps= new DesiredCapabilities();
+			DesiredCapabilities Caps= new DesiredCapabilities();
 			
 			HashMap<String, Object> browserstackOptions = new HashMap<String, Object>();
 			
 			//browserstackOptions.put("local", "true");
-			browserstackOptions.put("os", "Windows");
-			browserstackOptions.put("osVersion", "11");
-			browserstackOptions.put("browserVersion", "latest");
+			
+			if (DeviceType.equalsIgnoreCase("Desktop")) 
+			{
+				browserstackOptions.put("os", excelOperation.getTestData("BS_OS_Desktop", "BrowserStack_Config", "Data"));
+				browserstackOptions.put("osVersion", excelOperation.getTestData("BS_OSVersion_Desktop", "BrowserStack_Config", "Data"));
+			}
+			else if (DeviceType.equalsIgnoreCase("Mobile")) 
+			{
+				browserstackOptions.put("os", excelOperation.getTestData("BS_OS_Mobile", "BrowserStack_Config", "Data"));
+				browserstackOptions.put("osVersion", excelOperation.getTestData("BS_OSVersion_Mobile", "BrowserStack_Config", "Data"));
+				browserstackOptions.put("deviceName",excelOperation.getTestData("BS_DeviceName_Mobile", "BrowserStack_Config", "Data"));
+			}
+			
+			browserstackOptions.put("browserVersion",excelOperation.getTestData("BS_BrowserVersion", "BrowserStack_Config", "Data"));
 			
 			browserstackOptions.put("projectName", "WPS RegressionSuite");
 			browserstackOptions.put("buildName", testSuiteName);
-			browserstackOptions.put("sessionName", "Sprint 35.4");
+			browserstackOptions.put("sessionName", excelOperation.getTestData("BS_BuildNo", "BrowserStack_Config", "Data"));
 			//browserstackOptions.put("name", testSuiteName);
 			
-			browserstackOptions.put("debug", "true"); // to enable visual logs
+			browserstackOptions.put("interactiveDebugging", "true"); // Enable the Interactive session in runtime
+			browserstackOptions.put("debug", "true"); 		// to enable visual logs
 			browserstackOptions.put("consoleLogs", "info"); 
 			browserstackOptions.put("networkLogs", "true"); // to enable network logs to be logged
 			
 			browserstackOptions.put("resolution", "1024x768");	
 			
-			browserstackOptions.put("idleTimeout", "900");
+			browserstackOptions.put("idleTimeout", "300"); // Default 90 sec and Max 300 Sec
 			
-			*/
-					
+							
 			//Set Firefox			
 			if(browser.equalsIgnoreCase("firefox")) 
-				//browserstackOptions.put("browser", browser);
-				driver= new FirefoxDriver();
+				browserstackOptions.put("browser", browser);
+				//driver= new FirefoxDriver();
 			
 			//Set Chrome		
 			else if(browser.equalsIgnoreCase("chrome"))
-				//browserstackOptions.put("browser", browser);
-				driver= new ChromeDriver();
+				browserstackOptions.put("browser", browser);
+				//driver= new ChromeDriver();
 							
 			// Set Edge 			
-			else if(browser.equalsIgnoreCase("Edge")) //browserstackOptions.put("browser", browser);
+			else if(browser.equalsIgnoreCase("Edge")) 
+				browserstackOptions.put("browser", browser);
 							
-			//Caps.setCapability("bstack:options", browserstackOptions);	
+			Caps.setCapability("bstack:options", browserstackOptions);	
 			
-			//driver= new RemoteWebDriver(new URL(URL), Caps);
+			driver= new RemoteWebDriver(new URL(URL), Caps);
 		
 			driver.manage().window().maximize();
 			driver.manage().deleteAllCookies();
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));			
 			
-			//Capabilities caps = ((RemoteWebDriver) driver).getCapabilities();
 			
-		/*	String browserName 		= browserstackOptions.get("browser").toString();							
+			String browserName 		= browserstackOptions.get("browser").toString();							
 			String browserVersion 	= browserstackOptions.get("browserVersion").toString();
 			String OS_Name			= browserstackOptions.get("os").toString();
 			String OS_Version 		= browserstackOptions.get("osVersion").toString();
-		*/
-			String browserName 		= "Chrome";							
-			String browserVersion 	= "111.0.5563.147";
-			String OS_Name			= "Windows";
-			String OS_Version 		= "10";
-			
-			
+	
+		
 			Reporting.summaryReportdesign(testSuiteName+"_ReportSummary_In_"+browserName+"_"+date,
 					browserName,browserVersion,OS_Name,OS_Version);
 			
 			LogTextFile.createTodayLog(testSuiteName+"_"+date);
-			
-			
+						
 		}
 		catch(Exception e){ System.out.println(e.getMessage());}
 
