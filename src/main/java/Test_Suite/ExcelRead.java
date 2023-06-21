bcba3ab4440905698564204aa01371369d507dda
Bindu <cbindusree@wiley.com>
2023-06-21 11:15:29 +0530

    testPush

--- src/main/java/Test_Suite/ExcelRead.java
+++ src/main/java/Test_Suite/ExcelRead.java
@@ -1,6 +1,11 @@
 package Test_Suite;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.lang.reflect.Method;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.io.*;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -8,43 +13,80 @@ import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-public class ExcelRead {
+import utilities.CaptureScreenshot;
+import utilities.CommonMethods;
+import utilities.DriverModule;
+import utilities.LogTextFile;
+import utilities.Reporting;
+import utilities.StatusDetails;
 
-	public static void main(String[] args) throws Exception {
-		try {
 
-		String excel = "C:\\Users\\shafali\\CategoryLanding_1\\Ecommerce_QA\\Test Data\\Automation_Datasheet.xlsx";
-		FileInputStream file = new FileInputStream (excel);
+public class ExcelRead extends DriverModule {
+	
+	public static String startTime = new SimpleDateFormat("hhmmss").format(new Date());
+	public static String SS_path = Reporting.CreateExecutionScreenshotFolder(startTime);
 
-		XSSFWorkbook wb = new XSSFWorkbook(file);
-		XSSFSheet sheet = wb.getSheet("StoreFront_PDP");
-
-		int columnNumber=3;
-		for (Row row : sheet) {
-			Cell cell = row.getCell(columnNumber);
-			String cellValue=cell.getStringCellValue();
-			System.out.println(cellValue);			
-		}
-		wb.close();
-		file.close();
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-		/*int rows = sheet.getLastRowNum();
-		int columns = sheet.getRow(1).getLastCellNum();
-
-		for (int i=0;i<=rows;i++) {
-
-			XSSFRow row = sheet.getRow(i);
-			for (int j=0;j<=columns;j++) {
-				XSSFCell cell =	row.getCell(j);
-				System.out.print(cell);
-				System.out.print("|");
-			}
-		}
-		 */
-
+	@BeforeMethod
+	public void nameBefore(Method method) {
+		System.out.println("Test case: " + method.getName() + " execution started");
 	}
 
+	@AfterMethod
+	public void nameAfter(Method method) {
+		System.out.println("Test case: " + method.getName() + " execution completed");
+	}
+
+	@Test
+	public void TC02_ProductDisplayPage() throws Exception {
+		try {
+			Reporting.test = Reporting.extent.createTest("TC02_ProductDisplayPage");
+			LogTextFile.writeTestCaseStatus("TC02_ProductDisplayPage", "Test case");
+
+            String excel = "C:\\Ecommerce_QA\\Storefront\\Ecommerce_QA\\Test Data\\Automation_Test(10).xlsx";
+            FileInputStream file = new FileInputStream (excel);
+            XSSFWorkbook wb = new XSSFWorkbook(file);
+            XSSFSheet sheet = wb.getSheet("StoreFront_PDP");
+            String clp_solr = "-c-";
+            String clp_constructor = "-c2-";
+            int columnNumber=4;
+
+            for (Row row : sheet) {
+                Cell cell = row.getCell(columnNumber);
+                String cellValue=cell.getStringCellValue();
+                //System.out.println(cellValue);
+                DriverModule.driver.get(cellValue);
+                //Thread.sleep(8000);
+                //WebElement lastvalue= DriverModule.driver.findElement(By.xpath("//*[@id='breadcrumbStyle']/child::li['+i+']"));
+                WebElement lastvalue= DriverModule.driver.findElement(By.xpath("//*[@id='breadcrumbStyle']/child::li[3]"));
+                lastvalue.click();
+
+                String Solr_URL=DriverModule.driver.getCurrentUrl();
+                System.out.println(Solr_URL);
+                
+                PrintStream ps = new PrintStream(new File("C:\\URLS\\URL1.txt"));
+                System.setOut(ps);
+                ps.print(Solr_URL);
+                
+                
+                //StringBuffer stringBuffer = new StringBuffer();
+				//stringBuffer.append(Solr_URL.replace(clp_solr, clp_constructor));
+            }
+
+            wb.close();
+
+            file.close();
+
+        }catch (Exception e) {
+    		Reporting.updateTestReport("CLP did not get shifted from Solr to Constructor " + e.getMessage(),
+    				CaptureScreenshot.getScreenshot(SS_path), StatusDetails.FAIL);
+    		e.printStackTrace();
+    	}
+    }
 }
--- src/main/java/utilities/CommonMethods.java
+++ src/main/java/utilities/CommonMethods.java
@@ -1,5 +1,6 @@
 package utilities;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
